bfd9dd48ee14c6aaffdc50a54b16f717f87f7665
Asche <chris.asche@gmail.com>
2015-03-23 21:31:29 -0300

    dev

--- config/env/development.js
+++ config/env/development.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-    db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/amka',
-	app: {
+    db: 'mongodb://localhost/amka-dev',
+    app: {
 		title: 'amka - Development Environment'
 	},
 	mailer: {
